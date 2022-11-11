8b9252dc79b349720a6a6ce35da1c1522f88ef0f
Light <felipe33brr@gmail.com>
2022-11-11 19:12:43 -0300

    Estoque, Validade, Custo e Foto adicionados
    +Estoque
    +Validade
    +Custo
    +Foto
    -bibliotecas
    -pasta imagens
    -estilo css
    -editar
    -criar

--- index.php
+++ index.php
@@ -3,22 +3,21 @@
 <head>
     <?php
     require_once "connect.php";
-
     ?>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bibliotecas/style.css">
     <title>Tabela teste</title>
 </head>
 <body>
-    <div name='tabela' >
-        <table border='5'>
-        <tr><td>Nome<td>Ean
+    <div class='tabela'>
+        <table class='resultado'>
+        <h1>Título tabela</h1>
+        <tr><td>Foto<td>Nome<td>EAN<td>Tipo<td>Estoque <td>Validade <td> Custo Unitário <td>NCM<td>CEST<td> <a href='adicionar.php'><img src='bibliotecas/imagens/adicionar.png'></a>
         <?php
         $query = "SELECT * from banco"; 
         if($result = $banco->query($query)){
             while($obj = $result->fetch_object()){
-                echo "<tr><td> $obj->nome <td> $obj->ean";
+                echo "<tr><td><img class='mini' src='imagens/$obj->imagem'><td> $obj->nome <td> $obj->ean <td> $obj->tipo <td> $obj->estoque <td> $obj->validade <td> $obj->custo <td>$obj->ncm <td> $obj->cest <td> <a href='editar.php'><img src='bibliotecas/imagens/editar.png'></a>";
             }
         }
         ?>
