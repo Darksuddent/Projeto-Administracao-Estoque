d1010e29e8f8bad828607e7de08c1e861fd33b07
Light <felipe33brr@gmail.com>
2022-11-14 17:27:06 -0300

    Redirect automático quando editar ou criar um item

--- bibliotecas/editar.php
+++ bibliotecas/editar.php
@@ -22,21 +22,6 @@ if($nome==null || $ean == null){
         echo "<script>alert(Error: ".$sql."<br>".$banco->error.")</script>";
     }
 }
-
+header('Location: ../index.php');
+exit();
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Confirmar</title>
-</head>
-<body>
-    <a href='../index.php'><img src='icones/icoback.png'></a>
-</body>
-</html>
